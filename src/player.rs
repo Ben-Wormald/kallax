2b8b156449c7403780ec92f2fb76e4d309c51eb3
Ben-Wormald <benwormald95@gmail.com>
2024-02-14 16:15:40 +0000

    queue signals wip

--- src/player.rs
+++ src/player.rs
@@ -7,7 +7,7 @@ use crate::*;
 // https://docs.rs/axum-streams/latest/axum_streams/index.html
 
 struct Queue {
-    tracks: Vec<Arc<Track>>,
+    tracks: Vec<QueueTrack>,
     current: Option<usize>,
 }
 impl Queue {
@@ -24,11 +24,19 @@ impl Queue {
     }
 
     fn get_current(&self) -> Option<Arc<Track>> {
-        self.current.and_then(|index| self.tracks.get(index).map(|track| track.clone()))
+        self.current
+            .and_then(|index| self.tracks.get(index))
+            .map(|track| track.track.clone())
     }
 
     fn next(&mut self) {
-        self.current = self.current.and_then(|index| if index + 1 < self.tracks.len() { Some(index + 1) } else { None });
+        self.current = self.current.and_then(|index|
+            if index + 1 < self.tracks.len() {
+                Some(index + 1)
+            } else {
+                None
+            }
+        );
     }
 
     fn get_next(&mut self) -> Option<Arc<Track>> {
@@ -37,7 +45,21 @@ impl Queue {
     }
 
     fn queue(&mut self, track: Arc<Track>) {
-        self.tracks.push(track);
+        self.tracks.push(QueueTrack::new(track));
+    }
+}
+
+#[derive(Clone)]
+struct QueueTrack {
+    track: Arc<Track>,
+    signal: Arc<AtomicUsize>,
+}
+impl QueueTrack {
+    fn new(track: Arc<Track>) -> QueueTrack {
+        QueueTrack {
+            track,
+            signal: Arc::new(AtomicUsize::new(1)),
+        }
     }
 }
 
@@ -59,6 +81,27 @@ impl Player {
             _stream,
             _stream_handle: stream_handle,
         }
+    }
+
+    fn watch_signals(cx: &mut gpui::AppContext) {
+        let queue = Self::get_queue(cx);
+
+        cx.spawn(|mut cx| async move {
+            loop {
+                std::thread::sleep(Duration::from_secs(1));
+                for mut track in queue.tracks.clone() {
+                    if track.signal.load(std::sync::atomic::Ordering::SeqCst) == 0 {
+                        cx.update_global(|this: &mut Model<Player>, cx| {
+                            this.update(cx, |this, cx| {
+                                this.queue.next();
+                                cx.emit(Arc::new(PlaybackEvent::TrackEnded));
+                            });
+                        }).ok();
+                        break;
+                    }
+                }
+            }
+        }).detach();
     }
 
     fn get_sink(cx: &mut gpui::AppContext) -> Arc<Sink> {
@@ -83,33 +126,19 @@ impl Player {
 
     pub fn play(track: Arc<Track>, cx: &mut gpui::AppContext) {
         let sink = Self::get_sink(cx);
-        let queue = Self::get_queue(cx);
-        let signal = Arc::new(AtomicUsize::new(1));
 
         {
-            let track = track.clone();
-            let signal = signal.clone();
+            let track = QueueTrack::new(track.clone());
             cx.background_executor().spawn(async move {
-                let source = rodio::source::Done::new(Self::get_source(track), signal);
+                let source = rodio::source::Done::new(
+                    Self::get_source(track.track),
+                    track.signal,
+                );
                 sink.clear();
                 sink.append(source);
                 sink.play();
             }).detach();
         }
-
-        cx.spawn(|mut cx| async move {
-            loop {
-                std::thread::sleep(Duration::from_secs(1));
-                if signal.load(std::sync::atomic::Ordering::SeqCst) == 0 {
-                    cx.update_global(|this: &mut Model<Player>, cx| {
-                        this.update(cx, |_this, cx| {
-                            cx.emit(Arc::new(PlaybackEvent::TrackEnded));
-                        })
-                    }).ok();
-                    break;
-                }
-            }
-        }).detach();
 
         Self::emit(cx, PlaybackEvent::start(&track));
     }
