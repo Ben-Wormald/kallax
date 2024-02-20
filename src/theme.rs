8eeefb72de5da9ec9fa8c2a46b4c747cc83fefad
Ben Wormald <benwormald95@gmail.com>
2024-02-20 18:56:17 +0000

    colours wip

---
+++ src/theme.rs
@@ -0,0 +1,17 @@
+mod colours {
+    const WINTER: u32 = 0x333531;
+    const SMOTHER: u32 = 0x1f211f;
+    const YOUTH: u32 = 0xf2f4f3;
+    const STILL: u32 = 0xf2f4f3;
+    const LIFEFORMS: u32 = 0xf2f4f3;
+    const TOMORROW: u32 = 0xf2f4f3;
+    const HUMAN: u32 = 0xf2f4f3;
+    const TOUCH: u32 = 0xf2f4f3;
+    const AMSTERDAM: u32 = 0xf2f4f3;
+    const SHALLOWS: u32 = 0xf2f4f3;
+
+
+    const COLOUR_BG: u32 = 0x333531;
+    const COLOUR_BORDER: u32 = 0x1f211f;
+    const COLOUR_TEXT: u32 = 0xf2f4f3;
+}
