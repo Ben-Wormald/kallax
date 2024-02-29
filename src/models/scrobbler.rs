27b09589eb624549c69abbeabcdd60350d4a0ff6
Ben Wormald <benwormald95@gmail.com>
2024-02-29 20:31:39 +0000

    scrobbler wip

--- src/models/scrobbler.rs
+++ src/models/scrobbler.rs
@@ -1,6 +1,6 @@
 use gpui::{Model, ModelContext};
 use reqwest::Client;
-use std::env;
+use std::{env, sync::Arc};
 
 use crate::*;
 
@@ -9,7 +9,7 @@ type Mcx<'a> = ModelContext<'a, Scrobbler>;
 const API: &str = "http://ws.audioscrobbler.com/2.0/";
 
 pub struct Scrobbler {
-    client: Client,
+    client: Arc<Client>,
 }
 impl Scrobbler {
     pub fn new(playback: &Model<Playback>, cx: &mut Mcx) -> Scrobbler {
@@ -22,7 +22,7 @@ impl Scrobbler {
             }
         }).detach();
 
-        let client = Client::new();
+        let client = Arc::new(Client::new());
 
         Scrobbler {
             client,
@@ -30,6 +30,28 @@ impl Scrobbler {
     }
 
     fn update_now_playing(&self, cx: &mut Mcx) {
-        self.client.post(API);
+        let client = Arc::clone(&self.client);
+
+        cx.background_executor().spawn(async move {
+            let params = vec![
+                ("artist", "Kelly Moran"),
+                ("track", "Butterfly Phase"),
+                ("album", "Butterfly Phase"),
+                ("duration", "170"),
+                ("api_key", &env::var("LASTFM_API_KEY").unwrap()),
+                ("sk", &env::var("LASTFM_SESSION_KEY").unwrap()),
+                ("method", "track.updateNowPlaying"),
+                // ("format", "json"),
+            ];
+            client.post(API).body("hi").send().await.unwrap();
+        }).detach();
     }
+}
+
+fn sign<'a>(mut params: Vec<(&'a str, &'a str)>) -> Vec<(&'a str, &'a str)> {
+    params.sort_by(|a, b| a.0.cmp(b.0));
+
+    let param_str = params.iter().map(|(k, v)| format!("{k}{v}")).collect::<Vec<String>>().join("");
+
+    params
 }
