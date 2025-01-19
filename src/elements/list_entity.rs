9384751ca3320dd3e902a60147130c93765305c6
Ben Wormald <benwormald95@gmail.com>
2025-01-19 12:18:42 +0000

    list_entity flex

--- src/elements/list_entity.rs
+++ src/elements/list_entity.rs
@@ -9,49 +9,62 @@ pub fn list_entity(
 ) -> impl IntoElement {
     let children = match browse_context {
         BrowseContext::Album(track_number) => vec![
-            div().child(track_number.to_string()),
-            div().child(entity.name().to_string()),
+            row_column()
+                .child(track_number.to_string()),
+            row_column()
+                .child(entity.name().to_string()),
         ],
         BrowseContext::Artist => match entity {
             KallaxEntity::Album(album) => vec![
-                div().child(album.title.clone()),
+                row_column()
+                    .child(album.title.clone()),
             ],
             KallaxEntity::Track(track) => vec![
-                div().child(track.title.clone()),
-                div().child(cx.global::<Library>()
-                    .get_album(&track.album_id)
-                    .map_or(String::from(""), |album| album.title.clone())
-                ),
+                row_column()
+                    .child(track.title.clone()),
+                row_column()
+                    .child(cx.global::<Library>()
+                        .get_album(&track.album_id)
+                        .map_or(String::from(""), |album| album.title.clone())
+                    ),
             ],
             _ => unimplemented!(),
         },
         BrowseContext::Search => match entity {
             KallaxEntity::Artist(artist) => vec![
-                div().child(artist.name.clone()),
+                row_column()
+                    .child(artist.name.clone()),
             ],
             KallaxEntity::Album(album) => vec![
-                div().child(album.title.clone()),
-                div().child(cx.global::<Library>()
-                    .get_artist(&album.artist_id)
-                    .map_or(String::from(""), |artist| artist.name.clone())
-                ),
+                row_column()
+                    .child(album.title.clone()),
+                row_column()
+                    .child(cx.global::<Library>()
+                        .get_artist(&album.artist_id)
+                        .map_or(String::from(""), |artist| artist.name.clone())
+                    ),
             ],
             KallaxEntity::Track(track) => vec![
-                div().child(track.title.clone()),
-                div().child(cx.global::<Library>()
-                    .get_album(&track.album_id)
-                    .map_or(String::from(""), |album| album.title.clone())
-                ),
-                div().child(cx.global::<Library>()
-                    .get_artist(&track.artist_id)
-                    .map_or(String::from(""), |artist| artist.name.clone())
-                ),
+                row_column()
+                    .child(track.title.clone()),
+                row_column()
+                    .child(cx.global::<Library>()
+                        .get_album(&track.album_id)
+                        .map_or(String::from(""), |album| album.title.clone())
+                    ),
+                row_column()
+                    .child(cx.global::<Library>()
+                        .get_artist(&track.artist_id)
+                        .map_or(String::from(""), |artist| artist.name.clone())
+                    ),
             ],
             _ => unimplemented!(),
         },
         BrowseContext::Playlist(track_number) => vec![
-            div().child(track_number.to_string()),
-            div().child(entity.name().to_string()),
+            row_column()
+                .child(track_number.to_string()),
+            row_column()
+                .child(entity.name().to_string()),
         ],
     };
 
@@ -71,4 +84,10 @@ pub fn list_entity(
         .on_click(on_click)
         .flex()
         .children(children)
+}
+
+fn row_column() -> Div {
+    div()
+        .flex_grow()
+        .flex_basis(px(1.))
 }
